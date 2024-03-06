40a6a184739baa998b698daec343044f3986ffc7
Mohammed-Aman16 <mohammedamann20@gmail.com>
2024-03-06 11:31:22 -0800

    Crausel addition

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import {Carousel} from 'react-responsive-carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css"
 function Home() {
   return (
     <>
-      <div className='main w-full min-h-screen bg-[url("https://img.freepik.com/free-photo/espresso-surrounded-by-scattered-coffee-beans-dark-surface_91128-4609.jpg?t=st=1709255583~exp=1709259183~hmac=18321b83d1eb3fdf2b07b30646ad988f81c89a61db45fbb6953e47f4552be80a&w=1380")] *: bg-no-repeat bg-cover bg-center flex justify-center items-center'>
+      <div className='main w-full h-[93vh] bg-[url("https://img.freepik.com/free-photo/espresso-surrounded-by-scattered-coffee-beans-dark-surface_91128-4609.jpg?t=st=1709255583~exp=1709259183~hmac=18321b83d1eb3fdf2b07b30646ad988f81c89a61db45fbb6953e47f4552be80a&w=1380")] *: bg-no-repeat bg-cover bg-center flex justify-center items-center'>
 
         <div className='text  text-white  w-full h-screen px-6 flex justify-between items-center'>
           <div className='w-[50%] h-[50%]'>
@@ -19,7 +20,25 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className='strip w-full h-[100px] bg-[#003B25]'></div>
+      <div className='strip w-full h-[70px] flex items-center justify-center bg-[#60695D]'>
+        <Carousel
+        infiniteLoop
+        autoPlay
+        showStatus = {false}
+        showArrows = {false}
+        showThumbs = {false}
+        showIndicators = {false}
+        interval={3000}
+        transitionTime={1000}
+        swipeScrollTolerance={50}
+        >
+          <div className='text text-white text-lg font-light'> Weekend Brunch     I    Saturday 9am - 2pm </div>
+          <div className='text text-white text-lg font-light'> Daily Deal    |   Coffee & Pastry $3.99 </div>
+        </Carousel>
+
+      </div>
+
+      <div className='w-full h-screen bg-white'></div>
     </>
   );
 }
