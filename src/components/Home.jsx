84e0c54437d896bd291c424ad132a0d2a8e10b58
Mohammed-Aman16 <mohammedamann20@gmail.com>
2024-03-08 06:00:46 -0800

    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -37,7 +37,14 @@ function Home() {
         </Carousel>
       </div>
       <div className='main2 w-full h-[70vh] flex'>
-        <div className=' w-full h-full bg-slate-100'>
+        <div className=' w-full h-full bg-slate-100 flex gap-6 justify-center items-center'>
+          <div className='flex flex-col gap-4 items-start justify-center w-full h-full px-40'>
+            <h1 className='text-7xl font-bold text-black '>Eat.</h1>
+            <h1 className='font-normal text-xl'>Breakfast, Lunch and Artisanal Pastries</h1>
+            <p className='font-light text-lg'>Experience the joy of dining out at our restaurant, where every meal is a culinary delight. Our menu features a tantalizing selection of dishes crafted with care and expertise, using only the freshest ingredients. Whether you're craving a hearty meal or a light bite, our menu has something for everyone. Sit back, relax, and enjoy the warm and inviting ambiance as our friendly staff takes care of you. </p>
+            <button className='py-4 px-5 border-[1px] border-black  hover:bg-[#60695D] hover:border-none hover:text-white transition-all ease-linear'>See More</button>
+          </div>
+
 
         </div>
         <div className=' w-full h-full bg-[url("https://static.wixstatic.com/media/c837a6_b12138462b31409aaf8029cf7a2dd7ed~mv2.jpg/v1/fill/w_953,h_590,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c837a6_b12138462b31409aaf8029cf7a2dd7ed~mv2.jpg")] bg-no-repeat bg-cover bg-center'>
@@ -49,19 +56,33 @@ function Home() {
 
         </div>
         <div className=' w-full h-full bg-slate-100'>
+          <div className='flex flex-col gap-4 items-start justify-center w-full h-full px-40'>
+            <h1 className='text-7xl font-bold text-black '>Drink. </h1>
+            <h1 className='font-normal text-xl'>The Freshest Cup in Town </h1>
+            <p className='font-light text-lg'>
+              Savor the rich aroma and exquisite flavor of freshly brewed coffee at our restaurant. Our baristas are dedicated to crafting the perfect cup, using premium beans sourced from around the world. Whether you prefer a classic espresso, a velvety cappuccino, or a refreshing iced latte, we have a wide range of coffee options to suit every taste.  </p>
+            <button className='py-4 px-5 border-[1px] border-black  hover:bg-[#60695D] hover:border-none hover:text-white transition-all ease-linear'>See More</button>
+          </div>
 
         </div>
 
       </div>
       <div className='main2 w-full h-[70vh] flex'>
         <div className=' w-full h-full bg-slate-100'>
+          <div className='flex flex-col gap-4 items-start justify-center w-full h-full px-40'>
+            <h1 className='text-7xl font-bold text-black '>Enjoy. </h1>
+            <h1 className='font-normal text-xl'>Make Yourself at Home </h1>
+            <p className='font-light text-lg'>
+              At our restaurant, we believe in more than just great food and drinks - we believe in creating an experience that allows you to truly enjoy yourself. From the moment you step through our doors, our goal is to transport you to a place where you can relax, unwind, and savor every moment.   </p>
+            <button className='py-4 px-5 border-[1px] border-black  hover:bg-[#60695D] hover:border-none hover:text-white transition-all ease-linear'>See More</button>
+          </div>
 
         </div>
         <div className=' w-full h-full bg-[url("https://static.wixstatic.com/media/c837a6_5d9b5804b420484aa112d37fa3ce3f45~mv2.jpg/v1/fill/w_953,h_590,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c837a6_5d9b5804b420484aa112d37fa3ce3f45~mv2.jpg")] bg-no-repeat bg-cover bg-center'>
 
         </div>
       </div>
-      
+
     </>
   );
 }
